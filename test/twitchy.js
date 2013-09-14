a1a12d28594548e617b52b36a3e952efc53b59fd
Tony Ivanov <telamohn@gmail.com>
2013-09-14 23:49:38 +0200

    Completed preliminary autentification ,beginning to port api calls

--- test/twitchy.js
+++ test/twitchy.js
@@ -12,16 +12,25 @@ describe('TwitchTV Library', function(){
 		should.exist(client);
 	});
   describe('OAuth2 service',function(done){
-  it('should be possible to authenticate', function(done){
-    should.exist(credentials);
-    should.exist(credentials.key);
-    should.exist(credentials.secret);
-    var client  = new Twitchy(credentials).auth(function(err,access_token, refresh_token,result){
-      should.not.exist(err);
-      should.exist(access_token);
-      done();
+    it('should be possible to authenticate', function(done){
+      should.exist(credentials);
+      should.exist(credentials.key);
+      should.exist(credentials.secret);
+      var client  = new Twitchy(credentials).auth(function(err,access_token, refresh_token,result){
+        console.log("AccessToken: "+access_token);
+        should.not.exist(err);
+        should.exist(access_token);
+        done();
+      });
+    });
+    it('should be possible to fetch a restricted resource',function(done){
+      var client  = new Twitchy(credentials).auth();
+      client.getChannel("numrii",function(err,res){
+        should.not.exist(err);
+        console.log(res);
+        done();
+      });
     });
   });
 
-  });
 });
--- twitchy.js
+++ twitchy.js
@@ -1,7 +1,11 @@
 var OAuth = require('oauth'),
   OAuth2 = OAuth.OAuth2,
   _ = require('underscore'),
-  http = require('http');
+  http = require('http'),
+  url = require('url'),
+  request = require('superagent');
+
+require('superagent-oauth')(request);
    
 var twitchy = function(opts){
   opts = _.extend( {
@@ -22,36 +26,72 @@ var twitchy = function(opts){
 
   // Auth function
   this.auth = function(callback){
+
+    if(opts.access_token){
+      // TODO: fix implementation of refreshtokens when an accesstoken age times out.
+      callback && callback(null,opts.access_token);
+      return this;
+    }
     // Start a temporary webserver
-    var authSrv = http.createServer(function(req,res){ 
-      debugger;
-      // Redirect user to authentication url.
-      var authUrl = oauth.getAuthorizeUrl({
-        redirect_uri: 'http://localhost:'+opts.httpPort,
-        response_type : 'code'
-      });
-      res.writeHead(302, {'Location': authUrl,});
-      res.end();
+    var authSrv = http.createServer(function(req,res){       
+      if(req.url.match(/^\/$/)){
+        // No code present, Redirect user to authentication url.
+        var authUrl = oauth.getAuthorizeUrl({
+          redirect_uri: 'http://localhost:'+opts.httpPort,
+          response_type : 'code',
+          scope: [
+            'user_read',
+            'user_blocks_edit',
+            'user_blocks_read',
+            'chat_login'
+          ]          
+        });
+        res.writeHead(302, {'Location': authUrl,});
+        res.end();
+      }else if(req.url.match(/^\/\?code=/)){
+        // We've got the code
+        var code = req.url.match(/^\/\?code=([^&]+)(?:&|$)/)[1];        
+        oauth.getOAuthAccessToken(code,{
+            'grant_type' : 'authorization_code',
+             scope: [
+              'user_read',
+              'user_blocks_edit',
+              'user_blocks_read',
+              'chat_login'
+             ],
+             redirect_uri: 'http://localhost:'+opts.httpPort,
+          },
+          function(err, access_token,refresh_token,result){
+            if(!err){
+              opts.access_token =access_token;
+              opts.refresh_token =refresh_token;            
+            }
+            callback && callback(err, access_token,refresh_token,result);
+          }
+          
+        );
+        res.end("");
+        //authSrv.close();
+      }
     });
-   
     authSrv.listen(opts.httpPort);
-    
     console.log("Now open your browser and point it to: http://localhost:4567");
-
-
-    /*oauth.getOAuthAccessToken('',{
-        'grant_type' : 'authorization_code',
-         scope: [
-          'user_read',
-          'user_blocks_edit',
-          'user_blocks_read',
-          'chat_login'
-         ]
-      },
-      callback
-    );*/
     return this;
   }
+
+  _get = function(url){
+    return request(opts.baseUrl+url).sign(oauth, opts.access_token, opts.secret);
+  };
+  /////////////
+  /// API Calls
+  /////////////
+
+  this.getChannel=function(name,cb){
+    _get("channels/"+name).end(function(err,res){cb && cb(err, res.body)});
+  };
+
+
+
 };
 module.exports= twitchy;
 
