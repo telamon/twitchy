f0b6ff132c476aae4e773914266c22f66fd353c6
Tony Ivanov <telamohn@gmail.com>
2013-09-14 21:32:20 +0200

    Switching computers

--- test/twitchy.js
+++ test/twitchy.js
@@ -1,6 +1,7 @@
 var path = require('path'),
   Twitchy = require(path.join(__dirname, '../','twitchy.js')),
-  should = require('should');
+  should = require('should'),
+  credentials = require(path.join(__dirname,'../','credentials.json'));
 
 describe('TwitchTV Library', function(){
 	it('should export an object',function(){
@@ -12,8 +13,12 @@ describe('TwitchTV Library', function(){
 	});
   describe('OAuth2 service',function(done){
   it('should be possible to authenticate', function(done){
-    var client  = new Twitchy().auth(function(err,access_token,refresh_token,results){
+    should.exist(credentials);
+    should.exist(credentials.key);
+    should.exist(credentials.secret);
+    var client  = new Twitchy(credentials).auth(function(err,access_token, refresh_token,result){
       should.not.exist(err);
+      should.exist(access_token);
       done();
     });
   });
--- twitchy.js
+++ twitchy.js
@@ -1,23 +1,55 @@
 var OAuth = require('oauth'),
   OAuth2 = OAuth.OAuth2,
-  _ = require('underscore');
+  _ = require('underscore'),
+  http = require('http');
    
 var twitchy = function(opts){
   opts = _.extend( {
-    baseurl: 'http://api.justin.tv/oauth/',
-    debug : true 
+    baseUrl: 'https://api.twitch.tv/kraken/',
+    key: 'placeholder',
+    secret: 'placeholder', 
+    debug : true,
+    httpPort : 4567
   }, opts || {});
   
-  var oauth = new OAuth2( opts.key , opts.secret, opts.baseUrl, null ,'oauth2/token',null);
+  var oauth = new OAuth2( 
+    opts.key , opts.secret, opts.baseUrl,
+    'oauth2/authorize',
+    'oauth2/token',
+    { 'Accept' : 'application/vnd.twitchtv.v2+json'}
+  );
 
 
   // Auth function
   this.auth = function(callback){
-    oauth.getOAuthAccessToken('',{
-        'grant_type' : 'client_credentials'
+    // Start a temporary webserver
+    var authSrv = http.createServer(function(req,res){ 
+      debugger;
+      // Redirect user to authentication url.
+      var authUrl = oauth.getAuthorizeUrl({
+        redirect_uri: 'http://localhost:'+opts.httpPort,
+        response_type : 'code'
+      });
+      res.writeHead(302, {'Location': authUrl,});
+      res.end();
+    });
+   
+    authSrv.listen(opts.httpPort);
+    
+    console.log("Now open your browser and point it to: http://localhost:4567");
+
+
+    /*oauth.getOAuthAccessToken('',{
+        'grant_type' : 'authorization_code',
+         scope: [
+          'user_read',
+          'user_blocks_edit',
+          'user_blocks_read',
+          'chat_login'
+         ]
       },
       callback
-    );
+    );*/
     return this;
   }
 };
